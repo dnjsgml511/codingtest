996ecbe67f3495d7ab7507ffb7824fbf08c5de2e
dnjsgml511 <dnjsgml511@naver.com>
2022-03-17 17:34:27 +0900

    조이스틱 문제

---
+++ Test/src/com/test/test/Joystick.java
@@ -0,0 +1,20 @@
+package com.test.test;
+
+public class Joystick {
+
+	public static void main(String[] args) {
+		String name = "JEROEN";
+		int sol = sol(name);
+		
+		System.out.println();
+		System.out.println(sol);
+	}
+
+	private static int sol(String name) {
+
+		System.out.println(name);
+		
+		return 0;
+	}
+
+}
