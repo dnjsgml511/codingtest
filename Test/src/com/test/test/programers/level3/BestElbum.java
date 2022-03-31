f650bfeda9f32f9eae43dadc130292202cb0a8a6
dnjsgml511 <dnjsgml511@naver.com>
2022-03-31 12:02:22 +0900

    코테

--- Test/src/com/test/test/programers/level3/BestElbum.java
+++ Test/src/com/test/test/programers/level3/BestElbum.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BestElbum {
 	public static void main(String[] args) {
@@ -23,70 +22,52 @@ public class BestElbum {
 	private static int[] sol(String[] genres, int[] plays) {
 
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
-
 		for (int i = 0; i < genres.length; i++) {
-			int count = map.getOrDefault(genres[i], 0) + plays[i];
-			map.put(genres[i], count);
+			int num = map.getOrDefault(genres[i], 0) + plays[i];
+			map.put(genres[i], num);
 		}
 
-		List<String> list = Arrays.stream(genres).distinct().collect(Collectors.toList());
+		List<Vo> list = new ArrayList<Vo>();
+		for (int i = 0; i < genres.length; i++) {
+			list.add(new Vo(genres[i], plays[i], i, map.getOrDefault(genres[i], 0)));
+		}
+		Collections.sort(list, (a, b) -> {
+			return b.sum - a.sum;
+		});
 
 		Collections.sort(list, (a, b) -> {
-			return map.get(a) - map.get(b);
+			return a.sum == b.sum ? b.play - a.play : 0;
 		});
 
-		List<Vo> voList = new ArrayList<Vo>();
-		for (int i = 0; i < genres.length; i++) {
-			voList.add(new Vo(genres[i], plays[i], i));
-		}
-
+		String tmp = "";
+		boolean flag = true;
+		List<Integer> indexs = new ArrayList<Integer>();
 		for (int i = 0; i < list.size(); i++) {
-			for (int j = 0; j < voList.size(); j++) {
-				if (voList.get(j).genre.equals(list.get(i))) {
-					Vo vo = voList.remove(j);
-					voList.add(0, vo);
-				}
-			}
+			if (!tmp.equals(list.get(i).genre)) {
+				indexs.add(list.get(i).index);
+				flag = true;
+			} else if (flag) {
+				flag = false;
+				indexs.add(list.get(i).index);
+			} 
+			tmp = list.get(i).genre;
 		}
 
-		Collections.sort(voList, (a, b) -> {
-			return a.genre.equals(b.genre) ? b.play - a.play : 0;
-		});
-
-		List<Integer> intList = new ArrayList<Integer>();
-		int start = 0;
-		for (int i = list.size() - 1; i >= 0; i--) {
-			String tmp = list.get(i);
-			for (int j = start; j < voList.size() - 1; j++) {
-				if (voList.get(j).genre.equals(tmp)) {
-					intList.add(voList.get(j).index);
-				} else {
-					start = j;
-					break;
-				}
-			}
-		}
-
-		return intList.stream().mapToInt(i->i).toArray();
+		return indexs.stream().mapToInt(i->i).toArray();
 	}
 
-	private static class Vo {
+	public static class Vo {
 		String genre;
 		int play;
 		int index;
+		int sum;
 
-		public Vo() {
-		}
-
-		public Vo(String genre, int play, int index) {
+		public Vo(String genre, int play, int index, int sum) {
 			this.genre = genre;
 			this.play = play;
 			this.index = index;
-		}
-
-		@Override
-		public String toString() {
-			return "vo [genre=" + genre + ", play=" + play + ", index=" + index + "]";
+			this.sum = sum;
 		}
 	}
+
 }
