f9a93b94e8dd209e7a85ce520e535d39717f63a0
dnjsgml511 <dnjsgml511@naver.com>
2022-05-02 17:45:23 +0900

    코태

--- Test/src/com/test/test/programers/level2/MenuRenewal.java
+++ Test/src/com/test/test/programers/level2/MenuRenewal.java
@@ -2,9 +2,13 @@ package com.test.test.programers.level2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MenuRenewal {
 
@@ -13,14 +17,6 @@ public class MenuRenewal {
 		int[] course = { 2, 3, 4 };
 //		String[] result = { "AC", "ACDE", "BCFG", "CDE" };
 
-//		String[] orders = { "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" };
-//		int[] course = { 2, 3, 5 };
-//		String[] result = { "ACD", "AD", "ADE", "CD", "XYZ" };
-
-//		String[] orders = { "XYZ", "XWY", "WXA" };
-//		int[] course = { 2, 3, 4 };
-//		String[] result = { "WX", "XY" };
-
 		String[] sol = sol(orders, course);
 
 		System.out.println();
@@ -28,52 +24,135 @@ public class MenuRenewal {
 
 	}
 
-	static List<String> strings;
+	static HashMap<String, Integer> map;
+
+	static void comb(String str, StringBuilder sb, int idx, int cnt, int n) {
+		if (cnt == n) {
+			map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+			return;
+		}
+		for (int i = idx; i < str.length(); i++) {
+			sb.append(str.charAt(i));
+			comb(str, sb, i + 1, cnt + 1, n);
+			sb.delete(cnt, cnt + 1);
+		}
+	}
 
 	static String[] sol(String[] orders, int[] course) {
 
-		strings = new ArrayList<String>();
+		ArrayList<String> answer = new ArrayList<>();
+		
+		System.out.println(Arrays.toString(orders));
+		System.out.println(Arrays.toString(course));
+		System.out.println();
+		
+		for (int i = 0; i < orders.length; i++) {
+			char[] charArr = orders[i].toCharArray();
+			Arrays.sort(charArr);
+			orders[i] = String.valueOf(charArr);
+		}
 
-		List<String> list = new ArrayList<String>();
-		for (String order : orders) {
-			String[] split = order.split("");
-			for (String string : split) {
-				if (list.indexOf(string) == -1) {
-					list.add(string);
+		for (int i = 0; i < course.length; i++) {
+			map = new HashMap<>();
+			int max = Integer.MIN_VALUE;
+			for (int j = 0; j < orders.length; j++) {
+				StringBuilder sb = new StringBuilder();
+				if (course[i] <= orders[j].length()) {
+					comb(orders[j], sb, 0, 0, course[i]);
+				}
+			}
+			for(Entry<String, Integer> entry : map.entrySet()) {
+				max = Math.max(max, entry.getValue());
+			}
+			
+			for(Entry<String, Integer> entry : map.entrySet()) {
+				if(max >= 2 && entry.getValue() == max) {
+					answer.add(entry.getKey());
 				}
 			}
 		}
+		
+		Collections.sort(answer);
 
-		boolean[] visited = new boolean[list.size()];
-
-		for (int c : course) {
-			dfs(list, visited, 0, c);
-		}
-
-		for(int i = 0; i < strings.size(); i++) {
-			
-		}
+		System.out.println(answer);
 		
 		return null;
 	}
 
-	static void dfs(List<String> list, boolean[] visited, int start, int r) {
+	static String[] sol2(String[] orders, int[] course) {
+
+		List<String> list = new ArrayList<String>();
+		for (String data : orders) {
+			for (String indata : data.split("")) {
+				if (!list.contains(indata)) {
+					list.add(indata);
+				}
+			}
+		}
+		Collections.sort(list);
+
+		result = new ArrayList<String>();
+
+		int len = list.size();
+		boolean[] visited = new boolean[len];
+		for (int i = 2; i < len; i++) {
+			dfs(list, visited, 0, i);
+		}
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (String reg : result) {
+			for (String data : orders) {
+				if (data.matches(reg)) {
+					int count = map.getOrDefault(reg, 0) + 1;
+					map.put(reg, count);
+				}
+			}
+		}
+
+		for (Entry<String, Integer> data : map.entrySet()) {
+			if (data.getValue() < 2) {
+				boolean check = result.remove(data.getKey());
+			}
+		}
+
+		for (int i = 0; i < result.size(); i++) {
+			String reg = result.get(i);
+			for (int j = i + 1; j < result.size(); j++) {
+			}
+		}
+
+		return null;
+	}
+
+	static List<String> result;
+
+	static void dfs(List<String> arr, boolean[] visited, int start, int r) {
+
 		if (r == 0) {
 			StringBuilder sb = new StringBuilder();
 			sb.append(".*");
 			for (int i = 0; i < visited.length; i++) {
 				if (visited[i]) {
-					sb.append(list.get(i)).append(".*");
+					sb.append(arr.get(i)).append(".*");
 				}
 			}
-			strings.add(sb.toString());
+			result.add(sb.toString());
+
 		} else {
-			for (int i = start; i < list.size(); i++) {
+			for (int i = start; i < arr.size(); i++) {
 				visited[i] = true;
-				dfs(list, visited, i + 1, r - 1);
+				dfs(arr, visited, i + 1, r - 1);
 				visited[i] = false;
 			}
 		}
-	}
 
+	}
 }
+
+//String[] orders = { "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" };
+//int[] course = { 2, 3, 5 };
+//String[] result = { "ACD", "AD", "ADE", "CD", "XYZ" };
+
+//String[] orders = { "XYZ", "XWY", "WXA" };
+//int[] course = { 2, 3, 4 };
+//String[] result = { "WX", "XY" };
