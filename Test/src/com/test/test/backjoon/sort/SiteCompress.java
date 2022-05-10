2897d50e3cc80e9854e46b917e08b1f95308f645
dnjsgml511 <dnjsgml511@naver.com>
2022-05-10 17:44:26 +0900

    코테

--- Test/src/com/test/test/backjoon/sort/SiteCompress.java
+++ Test/src/com/test/test/backjoon/sort/SiteCompress.java
@@ -2,6 +2,7 @@ package com.test.test.backjoon.sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -21,17 +22,26 @@ public class SiteCompress {
 			int num = sc.nextInt();
 			list.add(num);
 			distinct.add(num);
+			rank.add(-1);
 		}
 		sc.close();
 
 		distinct = distinct.stream().distinct().collect(Collectors.toList());
 		Collections.sort(distinct);
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < distinct.size(); i++) {
+			map.put(distinct.get(i), i);
+		}
 		
-		System.out.println(distinct);
+		for(int i = 0; i < list.size(); i++) {
+			rank.set(i, map.get(list.get(i)));
+		}
 		
+		System.out.println(rank);
 	}
 }
 
 /**
-5 2 4 -10 4 -9
+ * 5 2 4 -10 4 -9
  */
