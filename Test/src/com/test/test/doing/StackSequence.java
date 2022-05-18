20d0b3ffaa86c4a99a2af4ad49cc38febf71223c
dnjsgml511 <dnjsgml511@naver.com>
2022-05-18 17:38:10 +0900

    코테

--- Test/src/com/test/test/doing/StackSequence.java
+++ Test/src/com/test/test/doing/StackSequence.java
@@ -2,10 +2,11 @@ package com.test.test.doing;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class StackSequence {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int loop = Integer.parseInt(br.readLine());
@@ -18,9 +19,26 @@ public class StackSequence {
 			min = Math.min(min, num);
 			arr[i] = num;
 		}
-		
-		int gap = ( max + min ) / loop;
+
+		int gap = (max + min) / loop;
 		int pop = max;
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			int num = arr[i];
+			if (num == pop) {
+				stack.pop();
+				System.out.println("-");
+				pop -= gap;
+				while(pop == stack.peek()) {
+					stack.pop();
+					pop -= gap;
+					System.out.println("-");
+				}
+			} else {
+				stack.push(num);
+				System.out.println("+");
+			}
+		}
 	}
 
 }
