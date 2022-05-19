8fef38fa67dd7ae7ec004e0bff2ae9ac58a0d85d
dnjsgml511 <dnjsgml511@naver.com>
2022-05-19 17:36:28 +0900

    코테

--- Test/src/com/test/test/doing/Thirty.java
+++ Test/src/com/test/test/doing/Thirty.java
@@ -4,20 +4,34 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Thirty {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		String[] nums = br.readLine().split("");
-				
+
 		Arrays.sort(nums);
+
+		StringBuilder sb = new StringBuilder();
+		for (String data : nums) {
+			sb.append(data);
+		}
+		sb.reverse();
+
+		BigInteger num = new BigInteger(sb.toString());
+		BigInteger thirty = new BigInteger("30");
 		
-		
-		
+		if(!num.remainder(thirty).equals(0)) {
+			bw.write("-1");
+		}else {
+			bw.write(sb.toString());
+		}
+
 		bw.flush();
 		bw.close();
 	}
