384afcb9b0c243f7502c671d954016897fe03d69
lwh <lwh@192.168.123.100>
2022-05-30 22:06:24 +0900

    코테

---
+++ Test/src/com/test/test/doing/Tree.java
@@ -0,0 +1,41 @@
+package com.test.test.doing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Tree {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int node = Integer.parseInt(br.readLine());
+		String[] line = br.readLine().split(" ");
+		int remove = Integer.parseInt(br.readLine());
+
+		int[] addnode = new int[node];
+		for (int i = 0; i < node; i++) {
+			addnode[i] = Integer.parseInt(line[i]);
+		}
+
+		bw.flush();
+		bw.close();
+	}
+
+	private class Node {
+		private int data;
+		private Node next;
+
+		public Node(int input) {
+			this.data = input;
+			this.next = null;
+		}
+		
+		public String toString() {
+			return String.valueOf(this.data);
+		}
+	}
+
+}
