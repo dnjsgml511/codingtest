d5e8f265877cf7f29fbcfe1b4ea781c91ab4724f
dnjsgml511 <dnjsgml511@naver.com>
2022-05-31 08:58:09 +0900

    코테

--- Test/src/com/test/test/doing/CompressionOfStrings.java
+++ Test/src/com/test/test/doing/CompressionOfStrings.java
@@ -1,17 +1,16 @@
 package com.test.test.doing;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class CompressionOfStrings {
 
 	public static void main(String[] args) throws Exception {
-//		String s = "aabbaccc";
-//		int answer = 7;
+		String s = "aabbaccc";
+		int answer = 7;
 //		String s = "ababcdcdababcdcd";
 //		int answer = 9;
-		String s = "abcabcdede";
-		int answer = 8;
+//		String s = "abcabcdede";
+//		int answer = 8;
 //		String s = "abcabcabcabcdededededede";
 //		int answer = 14;
 //		String s = "xababcdcdababcdcd";
@@ -41,21 +40,29 @@ public class CompressionOfStrings {
 
 		System.out.println(s);
 		int count = 0, depth = 0;
+		int len = s.length();
+
 		boolean flag = false;
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i <= s.length() - length * 2; i++) {
+			System.out.println(i);
 			String first = s.substring(i, i + length);
 			String second = s.substring(i + length, i + length * 2);
 
 			System.out.println(first + " / " + second + " = " + first.equals(second));
 
 			if (first.equals(second)) {
-				i += first.length();
+				i += first.length() * 2;
+				len -= first.length();
+			} else {
+
 			}
 		}
+
+		System.out.println(len);
 		System.out.println();
 		System.out.println();
 
-		min = Math.min(min, 0);
+		min = Math.min(min, len);
 	}
 }
